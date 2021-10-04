b1dcf5eb1e776fa4d2c12ed613c368d6f287b539
louisstanica <90300129+louisstanica@users.noreply.github.com>
2021-10-04 18:31:09 +0200

    Add files via upload

---
+++ portDiscover.sh
@@ -0,0 +1,15 @@
+
+
+#/bin/bash
+
+# ./portScan.sh <ip-address>
+
+if [ $1 ]; then
+	ip_address=$1
+	for port in $(seq 1 65535); do
+		timeout 10 bash -c "echo '' > /dev/tcp/$ip_address/$port" 2>/dev/null  && echo "[*]Port $port -open" &
+	done;  wait
+else
+	echo -e "\n[*] Uso: ./portScan.sh <ip-address>\n"
+	 exit 1
+fi
